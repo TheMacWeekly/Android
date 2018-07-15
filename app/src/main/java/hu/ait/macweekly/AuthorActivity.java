6fb983a070566f0e216362e56f0146e6e5d934cc
Sammy-F <sfritsch@macalester.edu>
2018-07-15 15:46:51 -0500

    Working on AuthorActivity. Current issue involves finding a way to query all articles for a given author.

---
+++ app/src/main/java/hu/ait/macweekly/AuthorActivity.java
@@ -0,0 +1,281 @@
+package hu.ait.macweekly;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import hu.ait.macweekly.adapter.SearchRecyclerAdapter;
+import hu.ait.macweekly.data.Article;
+import hu.ait.macweekly.data.GuestAuthor;
+import hu.ait.macweekly.listeners.ArticleViewClickListener;
+import hu.ait.macweekly.listeners.EndlessRecyclerViewScrollListener;
+
+public class AuthorActivity extends MacWeeklyApiActivity implements ArticleViewClickListener {
+
+    boolean showingNewsFeed = false;
+    String searchQuery = "";
+
+    // Keys
+    public static final String ARTICLE_AUTHOR_KEY = "articleAuthorKey";
+    public static final String AUTHOR_IMG_URL_KEY = "authorImgUrlKey";
+    public static final String AUTHOR_BIO_KEY = "authorBioKey";
+
+    // Constants
+    private final String LOG_TAG = "AuthorActivity - ";
+
+    // Members
+    private SearchRecyclerAdapter mArticleAdapter;
+    private EndlessRecyclerViewScrollListener mEndlessScrollListener;
+
+    // Views
+    @BindView(R.id.refresh_view) SwipeRefreshLayout mSwipeRefreshLayout;
+    @BindView(R.id.errorButton) Button mErrorButtonView;
+    @BindView(R.id.ivAuthor) ImageView mAuthorImageView;
+    @BindView(R.id.tvAuthorName) TextView mAuthorNameView;
+    @BindView(R.id.tvAuthorBio) TextView mAuthorBioView;
+
+    // Data
+    String mAuthorName;
+    String mAuthorBio;
+    String mAuthorImgUrl;
+
+    // Code
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initContentViews();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        prepareNewsAPI();
+
+        prepareContentViews();
+
+        setUpBackButton();
+    }
+
+    private void prepareContentViews() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+
+        mEndlessScrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view, int categoryId, String searchString) {
+                addArticles(page, categoryId, searchString);
+            }
+        };
+        EndlessRecyclerViewScrollListener.ParamManager paramManager = mEndlessScrollListener.getParamManager();
+
+        mArticleAdapter = new SearchRecyclerAdapter(getApplicationContext(), this, paramManager);
+        mArticleAdapter.setDataSet(new ArrayList<Article>());
+
+        mMainContent.setLayoutManager(linearLayoutManager);
+        mMainContent.setAdapter(mArticleAdapter);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                resetArticlesWithSearch(searchQuery);
+            }
+        });
+
+        mMainContent.addOnScrollListener(mEndlessScrollListener);
+
+        // Setting up search view
+        TextView.OnEditorActionListener editListener = new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+
+                // If the enter key was hit
+                // Note: There might be an error here. Not sure if this is the enter button for all
+                //       devices... I think it is, but if there is a bug with search enter key check here
+                if (i == EditorInfo.IME_ACTION_DONE ||
+                        (keyEvent != null && keyEvent.getAction() == KeyEvent.KEYCODE_ENTER)) {
+
+                    closeKeyboard();
+
+                    searchQuery = "";
+                    if(mSearchField.getText() != null) searchQuery = mSearchField.getText().toString();
+
+                    // If the search is valid
+                    if(isValidSearchQuery(searchQuery)) {
+                        resetArticlesWithSearch(searchQuery);
+                    } else {
+                        Log.d(LOG_TAG, "Search input with only spaces or empty is invalid");
+                        showScreenHint(SC_STARTSEARCH);
+                    }
+                }
+                return true;
+            }
+        };
+
+        mErrorButtonView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetArticlesWithSearch(searchQuery);
+                mSwipeRefreshLayout.setRefreshing(true);
+            }
+        });
+
+        mSearchField.setOnEditorActionListener(editListener);
+
+        showScreenHint(SC_STARTSEARCH);
+    }
+
+    private void closeKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    private boolean isValidSearchQuery(String query) {
+        if(query == null) return false;
+
+        String noWhiteSpace = query.replaceAll("\\s+","");
+        return !MacWeeklyUtils.isTextEmpty(noWhiteSpace);
+    }
+
+    private void initContentViews() {
+        setContentView(R.layout.activity_search);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ButterKnife.bind(this);
+    }
+
+    private void resetArticlesWithSearch(String searchString) {
+        if(isValidSearchQuery(searchString)) {
+            resetArticles(EndlessRecyclerViewScrollListener.ParamManager.NO_CATEGORY, searchString);
+        } else {
+            Log.d(LOG_TAG, "Search input with only spaces or empty is invalid");
+            showScreenHint(SC_STARTSEARCH);
+            mSwipeRefreshLayout.setRefreshing(false);
+        }
+    }
+
+    private void resetArticles(int categoryId, String searchString) {
+        mArticleAdapter.clearDataSet();
+        mArticleAdapter.notifyDataSetChanged();
+        showScreenHint(SC_MAINCONTENT);
+        mEndlessScrollListener.resetState(mMainContent, categoryId, searchString);
+        mEndlessScrollListener.startListener(mMainContent);
+    }
+
+    private void addArticles(int pageNum, int categoryId, String searchString) {
+        final int startSize = mArticleAdapter.getItemCount();
+        MacWeeklyApiActivity.ArticleCallback articleCallback = new MacWeeklyApiActivity.ArticleCallback() {
+            @Override
+            public void onSuccess(List<Article> articles) {
+                if (!showingNewsFeed) showScreenHint(SC_MAINCONTENT);
+                mArticleAdapter.addToDataSet(articles);
+                mArticleAdapter.notifyItemRangeChanged(startSize, ARTICLES_PER_CALL);
+            }
+
+            @Override
+            public void onFailure() {
+                if (mArticleAdapter.getDataSet().size() == 0) showScreenHint(SC_ERROR);
+            }
+        };
+        callNewsAPI(pageNum, categoryId, searchString, articleCallback);
+    }
+
+    private void populateDataMembers() {
+        Intent validIntent = getIntent();
+
+        String authorText = validIntent.getStringExtra(ARTICLE_AUTHOR_KEY);
+        if(authorText != null) mAuthorName = authorText; //Currently the mac weekly is a mess and when they don't have guest authors stored the title is stored here instead :/
+        else mAuthorName = "";
+
+        String authorBio = validIntent.getStringExtra(AUTHOR_BIO_KEY);
+        if(authorBio != null) mAuthorBio = authorBio;
+        else mAuthorBio = "";
+
+        String authorImgUrl = validIntent.getStringExtra(AUTHOR_IMG_URL_KEY);
+        if(authorImgUrl != null) {
+            mAuthorImgUrl = authorImgUrl;
+            mAuthorImageView.setVisibility(View.VISIBLE);
+        } else {
+            mAuthorImgUrl = "";
+            mAuthorImageView.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    void gotCallReturned() {
+        mSwipeRefreshLayout.setRefreshing(false);
+    }
+
+    @Override
+    void onResponseSuccess(int pageNum, List<Article> newArticles) {
+        if(!showingNewsFeed) showScreenHint(SC_MAINCONTENT);
+    }
+
+    @Override
+    void onResponseSuccessEmptyBody(int pageNum) {
+        if(mArticleAdapter.getItemCount() == 0) showScreenHint(SC_NORESULT);
+    }
+
+    @Override
+    void onResponseFailure(int pageNum) {
+        mSwipeRefreshLayout.setRefreshing(false);
+    }
+
+    @Override
+    public void articleViewClicked(View view, int position) {
+        showFullArticle(mArticleAdapter.getDataSet().get(position));
+    }
+
+    private void showFullArticle(Article targetArticle) {
+        // These attributes might be null or missing
+        String authorBio = "";
+        String authorName = "";
+        String authorImgUrl = "";
+        if(targetArticle.guestAuthor != null) {
+
+            GuestAuthor gAuthor = targetArticle.guestAuthor;
+
+            if(gAuthor.name != null) {
+                authorName = targetArticle.guestAuthor.name;
+            }
+
+            if(!MacWeeklyUtils.isTextEmpty(gAuthor.imgUrl)) {
+                authorImgUrl = gAuthor.imgUrl;
+            }
+
+            if(!MacWeeklyUtils.isTextEmpty(gAuthor.bio)){
+                authorBio = gAuthor.bio;
+            }
+        }
+
+        Intent articleIntent = new Intent(this, ArticleActivity.class);
+        articleIntent.putExtra(ArticleActivity.ARTICLE_CONTENT_KEY, targetArticle.content
+                .rendered);
+        articleIntent.putExtra(ArticleActivity.ARTICLE_DATE_KEY, targetArticle.date);
+        articleIntent.putExtra(ArticleActivity.ARTICLE_TITLE_KEY, targetArticle.title.rendered);
+        articleIntent.putExtra(ArticleActivity.ARTICLE_AUTHOR_KEY, authorName);
+        articleIntent.putExtra(ArticleActivity.ARTICLE_LINK_KEY, targetArticle.link);
+        articleIntent.putExtra(ArticleActivity.AUTHOR_IMG_URL_KEY, authorImgUrl);
+        articleIntent.putExtra(ArticleActivity.AUTHOR_BIO_KEY, authorBio);
+
+        startActivity(articleIntent);
+    }
+}
---
+++ app/src/main/java/hu/ait/macweekly/adapter/AuthorArticlesRecyclerAdapter.java
@@ -0,0 +1,131 @@
+package hu.ait.macweekly.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import hu.ait.macweekly.HTMLCompat;
+import hu.ait.macweekly.MacWeeklyUtils;
+import hu.ait.macweekly.R;
+import hu.ait.macweekly.data.Article;
+import hu.ait.macweekly.listeners.ArticleViewClickListener;
+import hu.ait.macweekly.listeners.EndlessRecyclerViewScrollListener;
+
+/**
+ * Created by Sammy Fritsche on 7/15/2018
+ */
+
+public class AuthorArticlesRecyclerAdapter extends RecyclerView.Adapter<RecyclerView
+        .ViewHolder> {
+
+    private static String LOG_TAG = "AUTHOR_ADAPTER - ";
+
+    private List<Article> mDataSet;
+    private Context mContext;
+    private static ArticleViewClickListener mArticleClickListener;
+    private EndlessRecyclerViewScrollListener.ParamManager mParamManager;
+
+    public AuthorArticlesRecyclerAdapter(Context context, ArticleViewClickListener articleClickListener,
+                                 EndlessRecyclerViewScrollListener.ParamManager paramManager) {
+        this.mContext = context;
+        this.mArticleClickListener = articleClickListener;
+        this.mParamManager = paramManager;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View articleView = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_article_view,
+                parent, false);
+        ArticleViewHolder articleViewHolder = new ArticleViewHolder(articleView);
+        return articleViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        ArticleViewHolder summaryViewHolder = (ArticleViewHolder) holder;
+        Article article = mDataSet.get(position);
+
+        // Load title
+        summaryViewHolder.title.setText(HTMLCompat.getInstance(mContext).fromHtml(article.title.rendered));
+
+        // Load excerpt
+        summaryViewHolder.sum.setText(HTMLCompat.getInstance(mContext).fromHtml(article.excerpt.rendered));
+
+        // Load the date, get it in a nice readable format first
+        String tempDateVal = article.date;
+        String correctlyFormattedDate = MacWeeklyUtils.formatDateTimeAgo(tempDateVal);
+        summaryViewHolder.date.setText(correctlyFormattedDate);
+
+        // Load guest author name if it exists. If not just put Mac Weekly as Author
+        String authorName;
+        if(article.guestAuthor == null || MacWeeklyUtils.isTextEmpty(article.guestAuthor.name)) authorName = "The Mac Weekly";
+        else authorName = article.guestAuthor.name;
+        summaryViewHolder.author.setText(HTMLCompat.getInstance(mContext).fromHtml(authorName));
+
+        // Load image thumbnail if url exists
+        if(!MacWeeklyUtils.isTextEmpty(article.normalThumbnailUrl)) {
+            String fullImgUrl = article.normalThumbnailUrl;
+            Glide.with(mContext).load(fullImgUrl).into(summaryViewHolder.thumbnailView);
+            summaryViewHolder.thumbnailView.setVisibility(View.VISIBLE);
+        } else {
+            Glide.clear(summaryViewHolder.thumbnailView);
+            summaryViewHolder.thumbnailView.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataSet.size();
+    }
+
+    public void setDataSet(List<Article> newArticles) {
+        mDataSet = newArticles;
+        notifyDataSetChanged();
+    }
+
+    public void addToDataSet(List<Article> newArticles) {
+        int oldIndexEnd = mDataSet.size() - 1;
+        mDataSet.addAll(newArticles);
+//        notifyDataSetChanged();
+        notifyItemRangeChanged(oldIndexEnd, mDataSet.size());
+    }
+
+    public class ArticleViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener{
+
+        @BindView(R.id.title) TextView title;
+        @BindView(R.id.summary) TextView sum;
+        @BindView(R.id.date) TextView date;
+        @BindView(R.id.authorField) TextView author;
+        @BindView(R.id.articleThumbnail) ImageView thumbnailView;
+
+        public ArticleViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            mArticleClickListener.articleViewClicked(view, this.getLayoutPosition());
+        }
+    }
+
+    public List<Article> getDataSet() {
+        return mDataSet;
+    }
+
+    public void clearDataSet() {
+        mDataSet.clear();
+    }
+}
